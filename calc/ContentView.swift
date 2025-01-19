9f3dc8d38c0f0c8486cbc0278d0d3d01ebfd0cd1
nyaago <nyaago69@gmail.com>
2025-01-19 12:41:07 +0900

    #9 add (temprary) toolbar

--- calc/ContentView.swift
+++ calc/ContentView.swift
@@ -16,23 +16,46 @@ struct ContentView: View {
     }
     
     var body: some View {
-        
-        VStack(alignment: .leading, spacing: 10.0) {
-            // 計算結果
-            ResultView(viewModel: self.viewModel)
-            // 入力
-            ExprInputView(viewModel: self.viewModel)
-            // 解析結果
-            PolishNotationView(viewModel: self.viewModel)
-
-            Button(action:  { onOk() }, label: {
-                Text("OK")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.textColor)
-            })
+        NavigationView {
+            VStack(alignment: .leading, spacing: 10.0) {
+                // 計算結果
+                ResultView(viewModel: self.viewModel)
+                // 入力
+                ExprInputView(viewModel: self.viewModel)
+                // 解析結果
+                PolishNotationView(viewModel: self.viewModel)
+            }
+            .padding(.horizontal, 10.0)
+            .padding(.bottom, 20.0)
+            .toolbar {
+                itemGroup
+            }.background(Color.black)
         }
-        .padding(.horizontal, 10.0)
+    }
+    
+    private var itemGroup: some ToolbarContent {
+        ToolbarItemGroup(placement: .bottomBar) {
+
+            Spacer()
+            Button(action: {
+                print("Button 1 tapped")
+            }) {
+                Image(systemName: "doc.text")
+            }
+            Spacer()
+            Button(action: {
+                print("Button 2 tapped")
+            }) {
+                Image(systemName: "list.bullet.rectangle")
+            }
+            Spacer()
+            Button(action: {
+                print("Button 3 tapped")
+            }) {
+                Image(systemName: "info")
+            }
+            Spacer()
+        }
     }
     
     func onOk() {
--- calc/views/BottomToolbarGroup.swift
+++ calc/views/BottomToolbarGroup.swift
@@ -4,5 +4,26 @@
 //
 //  Created by nyaago on 2025/01/18.
 //
+import SwiftUI
 
-struct 
+struct BottomToolbarGroup: View {
+    var body: some View {
+        NavigationStack {
+            Text("Hello, SwiftUI!")
+                .navigationTitle("Toolbar Example")
+                .toolbar {
+                    ToolbarItemGroup(placement: .topBarTrailing) {
+                        Button(action: {
+                            print("Button 1 tapped")
+                        }) {
+                            Image(systemName: "star")
+                        }
+                        Button(action: {
+                            print("Button 2 tapped")
+                        }) {
+                            Image(systemName: "heart")
+                        }
+                    }
+                }
+        }
+    }}
