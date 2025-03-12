31d9359ae9aa2437c966bcf86c8f590b520ade30
nyaago <nyaago69@gmail.com>
2025-03-13 00:07:03 +0900

    #17 Add Item button to sub view navigation bar (Temporary)

--- calc/page/DummyTextView.swift
+++ calc/page/DummyTextView.swift
@@ -8,20 +8,42 @@
 import SwiftUI
 
 struct DummyTextView: View {
+    @Environment(\.dismiss) private var dismiss
     var viewModel: CalcModel
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("Dummy")
-                .modifier(LabelTextModifier())
-            Text(viewModel.stringValue)
-                .multilineTextAlignment(.leading)
-                .lineLimit(nil)
-                .modifier(MultiLineTextModifier())
-                .frame(maxWidth: .infinity,
-                       minHeight: 50,
-                       maxHeight: .infinity,
-                       alignment: .topLeading)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Dummy")
+                    .modifier(LabelTextModifier())
+                Text(viewModel.stringValue)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(nil)
+                    .modifier(MultiLineTextModifier())
+                    .frame(maxWidth: .infinity,
+                           minHeight: 50,
+                           maxHeight: .infinity,
+                           alignment: .topLeading)
+            }
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button(action: {
+                        dismiss()
+                    },
+                    label: { Label("Save", systemImage: "arrowshape.down.circle") })
+                        
+                }
+                /*
+                 ToolbarItem(placement: .cancellationAction) {
+                 Button(action: {
+                 dismiss()
+                 },
+                 label: { Label("Back", systemImage: "arrow.backward")
+                 })
+                 }
+                 */
+            }
+            //.navigationBarBackButtonHidden(true)
         }
     }
 }
